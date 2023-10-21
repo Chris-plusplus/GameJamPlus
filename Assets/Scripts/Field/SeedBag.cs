1835c1306fac9989b576f2bb2a17124137551f32
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2023-10-21 12:53:47 +0200

    field

--- Assets/Scripts/Field/SeedBag.cs
+++ Assets/Scripts/Field/SeedBag.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class SeedBag : Liftable
 {
     [SerializeField] private SeedSO seedType;
-    [SerializeField] private float fadeSpeed = 0.05f;
+    //[SerializeField] private float fadeSpeed = 0.05f;
+    [SerializeField] private int seedCount;
 
     private Interactable interactable;
 
@@ -18,24 +19,34 @@ public class SeedBag : Liftable
 
     public override void Drop()
     {
-        base.Drop();
-        TryPlant();
+        if (!TryPlant())
+        {
+            base.Drop();
+        }
+        else if(seedCount <= 0)
+        {
+            base.Drop();
+            Destroy(gameObject);
+        }
     }
 
     private bool TryPlant() // zrobiæ tak by po ustawieniu FieldPatch,  FieldPatch zespawnowa³ planta (nowa klasa itd), a worek zosta³ w rêce
     {
         var target = LiftableHolder.SelectedObject;
-        if (target != null && target.TryGetComponent(out FieldPatch patch))
+        if (target != null && target.TryGetComponent(out FieldPatch fieldPatch))
         {
-            StartCoroutine(Plant(patch));
+            Seed seed = Instantiate(seedType.seed, transform.position, transform.rotation);
+            seed.PlantAt(fieldPatch);
+            --seedCount;
             return true;
         }
         return false;
     }
 
-    private IEnumerator Plant(FieldPatch patch)
+    /*
+    private IEnumerator Plant(FieldPatch fieldPatch)
     {
-        patch.SetPlant(this);
+        fieldPatch.isOccupied = true;
         interactable.enabled = false;
         myRigidbody.isKinematic = true;
         myRigidbody.useGravity = false;
@@ -43,23 +54,24 @@ public class SeedBag : Liftable
         var percent = 0f;
         while (percent < 1f)
         {
-            transform.position = Vector3.Lerp(startPos, patch.PlantPoint.position, percent);
+            transform.position = Vector3.Lerp(startPos, patch.SeedPoint.position, percent);
             percent += Time.deltaTime;
             yield return null;
         }
-        transform.position = patch.PlantPoint.position;
-
-        StartCoroutine(Fade(patch));
+        transform.position = fieldPatch.SeedPoint.position;
+        //patch.SetPlant(seedType.prefab);
+        StartCoroutine(Fade(fieldPatch));
     }
 
-    private IEnumerator Fade(FieldPatch patch)
+    private IEnumerator Fade(FieldPatch fieldPatch)
     {
         while (transform.localScale.x > 0)
         {
             transform.localScale -= fadeSpeed * Vector3.one;
             yield return null;
         }
-        patch.SetPlant(null);
+        fieldPatch.SetPlant(seedType.prefab);
         Destroy(gameObject);
     }
+    */
 }
