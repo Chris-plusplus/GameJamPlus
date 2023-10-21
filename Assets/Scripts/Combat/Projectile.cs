7ab6cab984aa09409c2bd01e9fc2b7a506ab6763
jbojarow <01178514@pw.edu.pl>
2023-10-21 15:57:32 +0200

    new map

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public Team target;
+    private Rigidbody rb;
+    [SerializeField] private float aimbot = 1f;
+
+    public Team targetTeam;
     public int damage;
+    public Transform targetTransform;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity += (targetTransform.position - transform.position).normalized *
+            aimbot / ((targetTransform.position - transform.position).sqrMagnitude + 1);
+        Debug.Log((targetTransform.position - transform.position).normalized *
+            aimbot / ((targetTransform.position - transform.position).sqrMagnitude + 1));
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         CombatEntity hitEntity;
         if (other.TryGetComponent<CombatEntity>(out hitEntity))
         {
-            if (hitEntity.team == target)
+            if (hitEntity.team == targetTeam)
             {
                 hitEntity.TakeDamage(damage);
                 Destroy(gameObject);
